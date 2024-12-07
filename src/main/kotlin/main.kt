2b364d5f3540e2419c6322f236cce63f232d821e
N4v1ds0n <damian.drostemx.de>
2024-12-07 01:18:21 +0100

    update

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    print("Hello KotlinTutorial.io")
+    val age : Int // Declaration of the variable age
+    age = 25      // Assignment of value 25
+    println(age)
+
+    var name : String
+    name = "Damian"
+    println(name)
+    name = "Alicia"
+    println(name)
 }
